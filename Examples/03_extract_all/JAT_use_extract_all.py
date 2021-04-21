42716411e8a4c803001fd6805590062bb9beeef9
christavanijzendoorn <christa.vanijzendoorn@gmail.com>
2021-04-21 19:10:42 +0200

    Update JAT_use_extract_all.py
    Removed saving and plotting of elevation data to reduce run time

--- Examples/03_extract_all/JAT_use_extract_all.py
+++ Examples/03_extract_all/JAT_use_extract_all.py
@@ -9,13 +9,12 @@ Created on Thu Feb  4 12:45:17 2021
 # PACKAGES
 ######################
 import yaml
-import pickle
 from JAT.Jarkus_Analysis_Toolbox import Transects, Extraction
 
 ######################
 # LOAD SETTINGS
 ######################
-config = yaml.safe_load(open("C:/Users/cijzendoornvan/OneDrive - Delft University of Technology/Documents/DuneForce/JARKUS/JAT/Examples/dune_toe_analysis/jarkus_03.yml"))
+config = yaml.safe_load(open("C:/Users/cijzendoornvan/OneDrive - Delft University of Technology/Documents/DuneForce/JARKUS/JAT/Examples/03_extract_all/jarkus_03.yml"))
 
 #%%###################
 # LOAD DATA
@@ -32,27 +31,9 @@ data = Transects(config)
 data.get_availability(config)
 
 # Those that were available are saved as follows: 
-print(data.transects_filtered)
-print(data.years_filtered)
+# print(data.transects_filtered)
+# print(data.years_filtered)
 
-#%%###################
-# SAVE + PLOT DATA
-######################
-# Save elevation dataframes for the available transects
-data.save_elevation_dataframes(config)
-
-# to reopen pickle file with elevation:
-transect = str(config['transects']['single'])
-elevation = pickle.load(open(config['outputdir'] + config['save locations']['DirA'] + str(transect) + '_elevation' + '.pickle','rb'))    
-
-#%%
-# Create elevation plots for the available transects - saved as png and pickle
-data.get_transect_plot(config)
-
-# to reopen pickle file with figure:
-figx = pickle.load(open(config['outputdir'] + config['save locations']['DirB'] + 'Transect_' + str(transect) + '.fig.pickle','rb'))    
-figx.show()
-    
 #%%###################################
 # EXTRACT CHARACTERISTICS PARAMETERS
 ######################################
