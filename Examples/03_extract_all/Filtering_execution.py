f752079c94b7f01a61629d21b2d90e17974e5f58
christavanijzendoorn <christa.vanijzendoorn@gmail.com>
2021-04-21 19:08:30 +0200

    Update Filtering_execution.py
    Included visualization and streamlined filtering

--- Examples/03_extract_all/Filtering_execution.py
+++ Examples/03_extract_all/Filtering_execution.py
@@ -10,21 +10,23 @@ Created on Wed Aug  5 07:12:48 2020
 # PACKAGES
 ######################
 import yaml
+import os
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt 
-from IPython import get_ipython
-from JAT.Jarkus_Analysis_Toolbox import Transects
+import matplotlib.gridspec as gridspec
+from JAT.Jarkus_Analysis_Toolbox import Transects, Extraction
 import JAT.Filtering_functions as Ff
-
-get_ipython().run_line_magic('matplotlib', 'auto') ## %matplotlib auto TO GET WINDOW FIGURE
 
 ######################
 # LOAD SETTINGS
 ######################
-config = yaml.safe_load(open(r"C:\Users\cijzendoornvan\OneDrive - Delft University of Technology\Documents\DuneForce\JARKUS\JAT\Examples\dune_toe_analysis\jarkus_03.yml"))
+config = yaml.safe_load(open(r"C:\Users\cijzendoornvan\OneDrive - Delft University of Technology\Documents\DuneForce\JARKUS\JAT\Examples\03_extract_all\jarkus_03.yml"))
 location_filter = yaml.safe_load(open(config['inputdir'] + config['data locations']['LocFilter']))
 plot_titles = yaml.safe_load(open(config['inputdir'] + config['data locations']['Titles'])) 
+DirFiltered = config['outputdir'] + config['save locations']['DirE']
+if os.path.isdir(DirFiltered) == False:
+    os.mkdir(DirFiltered)
 
 # Load jarkus dataset
 data = Transects(config)
@@ -45,14 +47,14 @@ def get_filtered_transects(variable, start_yr, end_yr):
 
     # dimension_nourished, dimension_not_nourished = Ff.nourishment_filter(config, dimension_filt)
 
-    # dimension_filt.to_pickle(config['outputdir'] + config['save locations']['DirE'] + variable + '_filtered_dataframe' + '.pickle')
+    dimension_filt.to_pickle(config['outputdir'] + config['save locations']['DirE'] + variable + '_filtered_dataframe' + '.pickle')
     # dimension_nourished.to_pickle(config['outputdir'] + config['save locations']['DirE'] + variable + '_nourished_dataframe' + '.pickle')
     # dimension_not_nourished.to_pickle(config['outputdir'] + config['save locations']['DirE'] + variable + '_not_nourished_dataframe' + '.pickle')
     
     return dimension_filt
 
 def get_distribution_plot(variable, dimension, figure_title, colorbar_label, start_yr, end_yr):
-
+   
     # Create an array with locations and an array with labels of the ticks
     ticks_x = [350, 1100, 1700]
     labels_x = ['Wadden Coast', 'Holland Coast', 'Delta Coast']
@@ -71,13 +73,19 @@ def get_distribution_plot(variable, dimension, figure_title, colorbar_label, sta
     range_value     = 2*stddev
     vmin            = average - range_value
     vmax            = average + range_value
-    print(vmax)
+    
+    average_through_space = dimension.mean(axis=0)
+    average_through_time = dimension.mean(axis=1)
+    vmin_avg = average_through_time.min()
+    vmax_avg = average_through_time.max()
     
     # Set-up of figure
-    fig = plt.figure() 
-    plt.title(figure_title, fontsize=24)
+    fig = plt.figure(figsize=(20,10)) 
+    fig.suptitle(figure_title, fontsize=24)
+    gs = gridspec.GridSpec(2, 2, width_ratios=[3, 1], height_ratios=[3,2]) 
     
     # PLOT TEMPORAL AND SPATIAL DISTRIBUTION OF VARIABLE
+    ax1 = fig.add_subplot(gs[0])
     cmap = plt.cm.get_cmap('Greens') # Define color use for colorbar
     colorplot = plt.pcolor(dimension.loc[start_yr:end_yr], vmin=vmin, vmax=vmax, cmap=cmap)
     # Set labels and ticks of x and y axis
@@ -88,138 +96,64 @@ def get_distribution_plot(variable, dimension, figure_title, colorbar_label, sta
     # plot boundaries between coastal regions
     plt.axvline(x=686, color='r') # Boundary kustvak 6 en 7, Den Helder, trsct 7000000, decameter 23417
     plt.axvline(x=1507, color='r')# Boundary kustvak 9 en 10, Hoek van Holland, trsct 10000000, decameter 35908
+    
+    # PLOT YEARLY AVERAGE OF VARIABLE
+    ax2 = fig.add_subplot(gs[1])
+    plt.plot(average_through_time, average_through_time.index, color='green')
+    #plt.scatter(average_through_time, average_through_time.index, c=average_through_time, cmap=cmap, vmin=vmin, vmax=vmax)
+    # Set labels and ticks of x and y axis
+    ticks_y = average_through_time.index[0::5]
+    plt.xlabel(colorbar_label)
+    plt.yticks(ticks_y, labels_y)
+    plt.tick_params(axis='y', which='both',length=5, labelsize = 16)
+    plt.xlim([vmin_avg-0.75*stddev, vmax_avg+0.75*stddev])
+    plt.tick_params(axis='x', which='both',length=5, labelsize = 16)
+    
+    # PLOT SPATIAL AVERAGES OF VARIABLE
+    ax3 = fig.add_subplot(gs[2])
+    plt.scatter(range(0, len(average_through_space)), average_through_space, c=average_through_space, cmap=cmap, vmin=vmin, vmax=vmax)
+    # Set labels and ticks of x and y axis
+    plt.xlim([0, len(average_through_space)])
+    plt.xticks(ticks_x, labels_x) 
+    plt.ylabel(colorbar_label)
+    plt.tick_params(axis='x', which='both',length=0, labelsize = 20)
+    plt.ylim([vmin, vmax])
+    plt.tick_params(axis='y', which='both',length=5, labelsize = 16)
+    plt.axvline(x=686, color='r') # Boundary kustvak 6 en 7, Den Helder, trsct 7000000, decameter 23417
+    plt.axvline(x=1507, color='r')# Boundary kustvak 9 en 10, Hoek van Holland, trsct 10000000, decameter 35908
 
-    # Plot colorbar
-    cbar = fig.colorbar(colorplot)
+    # Add colorbar
+    fig.subplots_adjust(right=0.88)
+    cbar_ax = fig.add_axes([0.90, 0.10, 0.02, 0.78])
+    cbar = fig.colorbar(colorplot, cax=cbar_ax)
     cbar.set_label(colorbar_label,size=18, labelpad = 20)
-    cbar.ax.tick_params(labelsize=16) 
-
+    cbar.ax.tick_params(labelsize=16)     
+    
     plt.tight_layout
     
     manager = plt.get_current_fig_manager()
     manager.window.showMaximized()
     plt.show()
 
-    # plt.savefig(config['outputdir'] + config['save locations']['DirE'] + variable + '_distribution' + '_plot.png')
+    plt.savefig(config['outputdir'] + config['save locations']['DirE'] + variable + '_distribution' + '_plot.png')
     # pickle.dump(fig, open(config['outputdir'] + config['save locations']['DirE'] + variable + '_distribution' + '_plot.fig.pickle', 'wb'))
+
+    plt.close()
 
 #%%##############################
 ####      EXECUTE            ####
 #################################
-start_yr = 1980 
+start_yr = 1965 
 end_yr = 2020
 
-dimension1 = get_filtered_transects('Dunetoe_y_der', start_yr, end_yr)
-figure_title = 'Alongshore and temporal variation of dune toe elevation (m)'
-colorbar_label = 'Dune toe elevation (m)'
-get_distribution_plot('Dunetoe_y_der', dimension1, figure_title, colorbar_label, start_yr, end_yr)
+extract = Extraction(data, config) # initalize the extra class 
+variables = extract.get_requested_variables() # get all variables that were requested (based on jarkus.yml file)
 
-dimension2 = get_filtered_transects('Dunetoe_x_der_normalized', start_yr, end_yr)
-
-dimension3 = get_filtered_transects('Dunetoe_y_pybeach_mix', start_yr, end_yr)
-dimension4 = get_filtered_transects('Dunetoe_x_pybeach_mix_normalized', start_yr, end_yr)
-
-
-
-
-
-
-
-# # Set-up of figure
-# fig = plt.figure(figsize=(25,7)) 
-
-# # fig.suptitle(figure_title, fontsize=26)
-
-# cmap = plt.cm.get_cmap(colormap_var) # Define color use for colorbar
-# # PLOT SPATIAL AVERAGES OF VARIABLE
-# colorplot = plt.scatter(range(0, len(average_through_space)), average_through_space, c=average_through_space, cmap=cmap, vmin=0, vmax=6)
-# # Set labels and ticks of x and y axis
-# plt.xlim([0, len(average_through_space)])
-# plt.xticks(ticks_x, labels_x) 
-# plt.ylabel('Elevation (m)', fontsize = 24)
-# plt.tick_params(axis='x', which='both',length=0, labelsize = 24)
-# plt.ylim([0, 6])
-# plt.tick_params(axis='y', which='both',length=5, labelsize = 24)
-# # plt.plot(range(0, len(average_through_space)),tidal_range, color = '#4169E1', label = 'Tidal range (m)', linewidth = 6) 
-# plt.plot(range(0, len(average_through_space)),MHW, color = '#4169E1', label = 'Mean High Water (m)', linewidth = 6) 
-
-# plt.axvline(x=686, color='r') # Boundary kustvak 6 en 7, Den Helder, trsct 7000000, decameter 23417
-# plt.axvline(x=1507, color='r')# Boundary kustvak 9 en 10, Hoek van Holland, trsct 10000000, decameter 35908
-
-# plt.legend(fontsize = 23, loc='upper left')
-
-# # Plot colorbar
-# cbar = fig.colorbar(colorplot)
-# cbar.set_label(colorbar_label,size=24, labelpad = 20)
-# cbar.ax.tick_params(labelsize=24) 
-
-# plt.tight_layout
-# plt.show()
-
-# filename2_pdf = 'Overview_DF_part1' + file_name + '.pdf'
-# filename2_eps = 'Overview_DF_part1' + file_name + '.eps'
-# filename2_png = 'Overview_DF_part1' + file_name + '.png'
-# plt.savefig(DirFigures + filename2_png, bbox_inches='tight')
-# plt.savefig(DirFigures + filename2_eps, format='eps', bbox_inches='tight')
-# plt.savefig(DirFigures + filename2_pdf, bbox_inches='tight')
-# print('saved figure')
-# #plt.close()
-
-
-
-
-# # Set-up of figure
-# fig2 = plt.figure() 
-
-# # fig2.suptitle(figure_title, fontsize=26)
-
-# # PLOT TEMPORAL AND SPATIAL DISTRIBUTION OF VARIABLE
-# cmap2 = plt.cm.get_cmap(colormap_var) # Define color use for colorbar
-# colorplot2 = plt.pcolor(DF_y_der, vmin=vmin, vmax=vmax, cmap=cmap2)
-# # Set labels and ticks of x and y axis
-# labels_y = [str(yr) for yr in DF_y_der.index][0::5]
-# plt.yticks(range(0, len(DF_y_der.index))[0::5], labels_y)
-# plt.tick_params(axis='y', which='both',length=5, labelsize = 22)
-# plt.xticks(ticks_x, labels_x) #rotation='vertical')
-# plt.tick_params(axis='x', which='both',length=0, labelsize = 24)
-# # plot boundaries between coastal regions
-# plt.axvline(x=686, color='r') # Boundary kustvak 6 en 7, Den Helder, trsct 7000000, decameter 23417
-# plt.axvline(x=1507, color='r')# Boundary kustvak 9 en 10, Hoek van Holland, trsct 10000000, decameter 35908
-
-# plt.legend(fontsize = 20)
-
-# # Plot colorbar
-# cbar2 = fig2.colorbar(colorplot2)
-# cbar2.set_label(colorbar_label,size=22, labelpad = 20)
-# cbar2.ax.tick_params(labelsize=20) 
-
-# plt.tight_layout
-
-# manager = plt.get_current_fig_manager()
-# manager.window.showMaximized()
-# plt.show()
-
-# filename2 = 'Overview_DF_part2' + file_name + '.png'
-# # filename3 = 'Overview_DF_part2' + file_name + '.eps'
-# # plt.savefig(DirFigures + filename3, format='eps')
-# plt.savefig(DirFigures + filename2)
-# print('saved figure')
-# #plt.close()
-
-
-
-# # PLOT YEARLY AVERAGE OF VARIABLE
-# ax2 = fig.add_subplot(gs[1])
-# plt.plot(average_through_time, average_through_time.index, color=colormap_var[:-1])
-# # plt.scatter(average_through_time, average_through_time.index, c=average_through_time, cmap=cmap, vmin=vmin, vmax=vmax)
-# # Set labels and ticks of x and y axis
-# ticks_y = average_through_time.index[0::5]
-# plt.xlabel(colorbar_label, fontsize = 20)
-# plt.yticks(ticks_y, labels_y)
-# plt.tick_params(axis='y', which='both',length=5, labelsize = 16)
-# plt.xlim([vmin_avg+0.75*stddev, vmax_avg-0.75*stddev])
-# plt.tick_params(axis='x', which='both',length=5, labelsize = 16)
-
-
-
+for variable in variables:
+    # variable = 'DuneTop_prim_x'
+    print(variable)
+    dimension = get_filtered_transects(variable, start_yr, end_yr)
+    figure_title = plot_titles[variable][0]
+    colorbar_label = plot_titles[variable][1]
+    get_distribution_plot(variable, dimension, figure_title, colorbar_label, start_yr, end_yr)
 
