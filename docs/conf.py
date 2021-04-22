bb1eba7643f3aaaa45e62ed27cb93b8b5cba5d99
christavanijzendoorn <christa.vanijzendoorn@gmail.com>
2021-04-22 20:22:20 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # See http://blog.rtwilson.com/how-to-make-your-sphinx-documentation-compile-with-readthedocs-when-youre-using-numpy-and-scipy/
 import mock
 
-MOCK_MODULES = ['numpy', 'pandas']
+MOCK_MODULES = ['numpy', 'pandas', 'math']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 	
