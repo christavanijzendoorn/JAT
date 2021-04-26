63dd8c61cb45b78b6c501a3d311822e6f85fbc39
christavanijzendoorn <christa.vanijzendoorn@gmail.com>
2021-04-26 21:54:01 +0200

    Provide documentation of functions

--- JAT/Filtering_functions.py
+++ JAT/Filtering_functions.py
@@ -6,17 +6,39 @@
 # """
 
 """
-Blabla
+Provides functions that allow filtering of the extracted characteristic 
+parameters.
 """
 
 import math
 import pandas as pd
 import numpy as np
 
-def bad_locations_filter(dimension, filter_file):
-    """
-    Blabla
-    """
+def locations_filter(dimension, filter_file):
+    """Filter out user-defined transects.
+    
+    Filter out locations that are specified by the user from a dataframe of a 
+    characteristic parameter. Default settings filter out locations like the 
+    Hondsbossche Dunes and Maasvlakte, redundant transects at the outer edges 
+    of the Wadden Islands, and dams in Zeeland.
+
+    Parameters
+    ----------
+    dimension : pd.dataframe
+        dataframe containing the values of a characteristic parameter through 
+        time and more multiple transect locations.
+    filter_file : dict
+        Includes a numbered list of sections that should be excluded. The 
+        first transect number represents the start of the section, the second 
+        transect number the end.
+        
+    Returns
+    -------
+    pd.dataframe
+        dimension_filt: dataframe containing the values of a 
+        characteristic parameter where filtered transects have been removed.
+        
+    """    
 
     # Filter dataframe
     dimension_filt = dimension.copy()
@@ -32,13 +54,30 @@ def bad_locations_filter(dimension, filter_file):
             
     return dimension_filt
 
-def bad_yrs_filter(dimension, begin_year, end_year):
-    """
-    Blabla
-    """
+def yrs_filter(dimension, begin_year, end_year):
+    """Filter out user-defined years.
     
-    # Filter dataframebased on a user-defined range. This should be based on analysis of the data availability in which this filter is NOT used.
-    # so use the availability_filter_years to see the years in which the availability is lowest.
+    Filter values of a characteristic parameter that are associated with a
+    range of years that is specified by the user. 
+
+    Parameters
+    ----------
+    dimension : pd.dataframe
+        dataframe containing the values of a characteristic parameter through 
+        time and more multiple transect locations.
+    begin_yr : int
+        Start year of the range that should be filtered
+    end_yr : int
+        End year of the range that should be filtered
+        
+    Returns
+    -------
+    pd.dataframe
+        dimension_filt: dataframe containing the values of a 
+        characteristic parameter where filtered years have been removed.
+        
+    """
+
     dimension_filt = dimension.copy()
     for i, row in dimension_filt.iterrows():
         if i < begin_year or i > end_year:
@@ -46,10 +85,28 @@ def bad_yrs_filter(dimension, begin_year, end_year):
             
     return dimension_filt
 
-def availability_filter_locations(config, dimension):
-    """
-    Blabla
-    """
+def availability_locations_filter(config, dimension):
+    """Filter out transects based on data availability.
+    
+    Filter out transects that have a data availability that is lower than the 
+    user-defined threshold. 
+
+    Parameters
+    ----------
+    config : dict
+        configuration file that includes the user defined availability 
+        threshold in percentage (filter2, locations)
+    dimension : pd.dataframe
+        dataframe containing the values of a characteristic parameter through 
+        time and more multiple transect locations.
+        
+    Returns
+    -------
+    pd.dataframe
+        dimension_filt: dataframe containing the values of a 
+        characteristic parameter where filtered transects have been removed.
+        
+    """ 
     
     availability_per_transect = pd.DataFrame({'transects': dimension.columns})
     availability_per_transect.set_index('transects', inplace=True)
@@ -66,10 +123,30 @@ def availability_filter_locations(config, dimension):
             
     return dimension_filt
 
-def availability_filter_years(config, dimension):
-    """
-    Blabla
-    """
+def availability_years_filter(config, dimension):
+    """Filter out years based on data availability.
+    
+    Filter out years that have a data availability that is lower than the 
+    user-defined threshold.  
+
+    Parameters
+    ----------
+    config : dict
+        configuration file that includes the user defined availability 
+        threshold in percentage (filter2, years)
+    dimension : pd.dataframe
+        dataframe containing the values of a characteristic parameter through 
+        time and for multiple transect locations.
+        
+    Returns
+    -------
+    pd.dataframe
+        dimension_filt: dataframe containing the values of a 
+        characteristic parameter where filtered years have been removed.
+    """ 
+    
+       #This should be based on analysis of the data availability in which this filter is NOT used.
+    # so use the availability_filter_years to see the years in which the availability is lowest.
     
     availability_per_year = pd.DataFrame({'years': dimension.index})
     availability_per_year.set_index('years', inplace=True)
@@ -86,10 +163,29 @@ def availability_filter_years(config, dimension):
             
     return dimension_filt
 
-def nourishment_filter(config, variable_dataframe):
-    """
-    Blabla
-    """
+def nourishment_filter(config, dimension):
+    """Split characteristic parameter values into nourished and not nourished 
+    transects.
+    
+    Parameters
+    ----------
+    config : dict
+        configuration file that includes the directory where the nourishment
+        database is stored.
+    dimension : pd.dataframe
+        dataframe containing the values of a characteristic parameter through 
+        time and more multiple transect locations.
+        
+    Returns
+    -------
+    pd.dataframe
+        nourished_dataframe: dataframe containing the values of a 
+        characteristic parameter of only transect that have been nourished.
+    pd.dataframe
+        not_nourished_dataframe: dataframe containing the values of a 
+        characteristic parameter of only transect that have not been nourished.
+        
+    """ 
     
     Nourishments = pd.read_excel(config['inputdir'] + config['data locations']['Nourishment'])
     filtered = []
@@ -99,17 +195,17 @@ def nourishment_filter(config, variable_dataframe):
         else:
             code_beginraai = int(row['KustVakNummer'] * 1000000 + row['BeginRaai'] * 100)
             code_eindraai = int(row['KustVakNummer'] * 1000000 + row['EindRaai'] * 100)
-            nourished_transects = [i for i in variable_dataframe.columns if int(i) >= code_beginraai and int(i) <= code_eindraai]
+            nourished_transects = [i for i in dimension.columns if int(i) >= code_beginraai and int(i) <= code_eindraai]
             filtered.extend(nourished_transects)
     filtered = set(filtered)
 
     # Filter dataframe
-    not_nourished_dataframe = variable_dataframe.copy()
+    not_nourished_dataframe = dimension.copy()
     for i, col in not_nourished_dataframe.iteritems():
         if i in filtered:
                 not_nourished_dataframe.loc[:, i] = np.nan
     
-    nourished_dataframe = variable_dataframe.copy()
+    nourished_dataframe = dimension.copy()
     for i, col in nourished_dataframe.iteritems():
         if i not in filtered:
                 nourished_dataframe.loc[:, i] = np.nan
